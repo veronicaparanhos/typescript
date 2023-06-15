236f6e05b75164a1dd339f0741193097747f3852
Verônica Paranhos <veronicahp@gmail.com>
2023-06-15 15:57:59 -0300

    subindo os últimos arquivos do curso de TS 2

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,14 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
-import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  controller.adiciona();
-});
-
-// const negociacoesView = new NegociacoesView();
-// const template = negociacoesView.template;
-// console.log(template);
+if (form) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    controller.adiciona();
+  });
+} else {
+  throw Error(
+    "Não foi possível inicializar a aplicação. Verifique se o form existe."
+  );
+}
